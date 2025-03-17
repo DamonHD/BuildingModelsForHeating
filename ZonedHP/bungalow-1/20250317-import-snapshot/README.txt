e89e4ed1299d72cf64f00720605559783170b8cf
DamonHD <d@hd.org>
2025-03-17 15:10:13 +0000

    Script and output captured.

--- ZonedHP/bungalow-1/20250317-import-snapshot/README.txt
+++ ZonedHP/bungalow-1/20250317-import-snapshot/README.txt
@@ -1,1 +1,5 @@
 IDF file imported from personal repository 2025-03-17.
+
+Script used to run the model.
+
+Output HTML from EnergyPlus.
---
+++ ZonedHP/bungalow-1/20250317-import-snapshot/run-2-GBRBirm.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# bungalow-1.idf design day.
+
+exec /Applications/EnergyPlus-24-2-0/energyplus -d out-2-GBRBirm -w ../20250114-model-behaviour/weather/GBR_Birmingham.035340_IWEC.epw bungalow-1.idf
