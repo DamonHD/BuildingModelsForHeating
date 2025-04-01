d5c52802ee995a006f372b70a017cdc31340101b
DamonHD <d@hd.org>
2025-04-01 15:15:06 +0100

    Trimmed dd script.

--- ZonedHP/bungalow-1/run-dd.sh
+++ ZonedHP/bungalow-1/run-dd.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Run bungalow-1.idf for winter design day.
 
-if [ "" = "${WEATHERDIR}" ]; then
-    echo "ERROR: please set WEATHERDIR" 1>&2
-    exit 1
-fi
+#if [ "" = "${WEATHERDIR}" ]; then
+#    echo "ERROR: please set WEATHERDIR" 1>&2
+#    exit 1
+#fi
 
-exec /Applications/EnergyPlus-24-2-0/energyplus -d out-dd -D -w ${WEATHERDIR}/GBR_Birmingham.035340_IWEC.epw bungalow-1.idf
+exec /Applications/EnergyPlus-24-2-0/energyplus -d out-dd -D bungalow-1.idf
