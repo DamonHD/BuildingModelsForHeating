102dcf39a73a0ea6435139c603ebec8a9d6472c6
DamonHD <d@hd.org>
2025-05-08 16:56:57 +0100

    Creating bungalow-1-diurnal...

---
+++ ZonedHP/bungalow-1-diurnal/run-GBRBirm.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Run bungalow-1.idf for a whole year with UK representative weather.
+
+if [ "" = "${WEATHERDIR}" ]; then
+    echo "ERROR: please set WEATHERDIR" 1>&2
+    exit 1
+fi
+
+exec /Applications/EnergyPlus-24-2-0/energyplus -d out-GBRBirm -w ${WEATHERDIR}/GBR_Birmingham.035340_IWEC.epw bungalow-1.idf
