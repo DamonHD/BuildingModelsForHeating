b8b69388b388f1577f8189dd6dfd73b736b3e1b7
Deniz Erbilgin <deniz.erbilgin@gmail.com>
2025-07-04 16:48:22 +0100

    more cases

--- ZonedHP/bungalow-2/test_bungalow_2.py
+++ ZonedHP/bungalow-2/test_bungalow_2.py
@@ -9,6 +9,16 @@ try:
 except ImportError:
     pass
 
+
+DATA_DIRS = [
+    "out-dd-AAAA-LC",
+    "out-dd-ABAB-LC",
+    "out-dd-AABB-LC",
+    "out-dd-AAAA-WC",
+    "out-dd-ABAB-WC",
+    "out-dd-AABB-WC",
+]
+
 CONVERGENCE_ERR = "   ** Warning ** SimHVAC: Maximum iterations (20) exceeded for all HVAC loops, at WINTER DESIGN DAY IN (MID) UK,"
 A_ROOM_TEMP_C = 21.0
 A_ROOM_SAG_C = 20.0
@@ -23,17 +33,13 @@ z3_heating_rate = "Z3 RADIATOR:Baseboard Total Heating Rate [W](Hourly)"
 z4_heating_rate = "Z4 RADIATOR:Baseboard Total Heating Rate [W](Hourly)"
 heat_pump_heating_rate = "HEAT PUMP:Heat Pump Load Side Heat Transfer Rate [W](Hourly)"
 water_pump_heating_rate = "SUPPLY PUMP:Pump Fluid Heat Gain Rate [W](Hourly)"
+baseboard_heating_rate = (
+    "Baseboard Total Heating Rate All Zones:PythonPlugin:OutputVariable [W](Hourly)"
+)
 
 
 def float_near(a, b, err):
     return abs(a - b) < err
-
-
-def has_convergence_error(file):
-    with open(file) as f:
-        for line in f.readlines():
-            if line.startswith(CONVERGENCE_ERR):
-                return line
 
 
 def read_eplus_csv(file):
@@ -52,20 +58,25 @@ def read_eplus_csv(file):
         }
 
 
-def test_AAAA_converges():
-    assert not has_convergence_error("out-dd-AAAA/eplusout.err")
+sim_results = {d: read_eplus_csv(f"{d}/eplusout.csv") for d in DATA_DIRS}
 
 
-def test_AABB_converges():
-    assert not has_convergence_error("out-dd-AABB/eplusout.err")
+def test_converges():
+    def has_convergence_error(file):
+        with open(file) as f:
+            for line in f.readlines():
+                if line.startswith(CONVERGENCE_ERR):
+                    return line
+        return None
+
+    result = ((d, has_convergence_error(f"{d}/eplusout.err")) for d in DATA_DIRS)
+    result = [r for r in result if r[1] is not None]
+
+    assert len(result) == 0, result
 
 
-def test_ABAB_converges():
-    assert not has_convergence_error("out-dd-ABAB/eplusout.err")
-
-
-def test_AAAA_heatflows_balance():
-    results = read_eplus_csv("out-dd-AAAA/eplusout.csv")
+def test_AAAA_LC_heatflows_balance():
+    results = sim_results["out-dd-AAAA-LC"]
     total_heating_supply = (
         results[heat_pump_heating_rate] + results[water_pump_heating_rate]
     )
@@ -75,12 +86,14 @@ def test_AAAA_heatflows_balance():
         + results[z3_heating_rate]
         + results[z4_heating_rate]
     )
+    total_baseboard_heating_rate = results[baseboard_heating_rate]
 
+    assert float_near(total_heating_demand, total_baseboard_heating_rate, 0.1)
     assert float_near(total_heating_supply, total_heating_demand, 1.0)
 
 
-def test_AABB_heatflows_balance():
-    results = read_eplus_csv("out-dd-AABB/eplusout.csv")
+def test_AAAA_WC_heatflows_balance():
+    results = sim_results["out-dd-AAAA-WC"]
     total_heating_supply = (
         results[heat_pump_heating_rate] + results[water_pump_heating_rate]
     )
@@ -90,12 +103,14 @@ def test_AABB_heatflows_balance():
         + results[z3_heating_rate]
         + results[z4_heating_rate]
     )
+    total_baseboard_heating_rate = results[baseboard_heating_rate]
 
+    assert float_near(total_heating_demand, total_baseboard_heating_rate, 0.1)
     assert float_near(total_heating_supply, total_heating_demand, 1.0)
 
 
-def test_ABAB_heatflows_balance():
-    results = read_eplus_csv("out-dd-ABAB/eplusout.csv")
+def test_AABB_LC_heatflows_balance():
+    results = sim_results["out-dd-AABB-LC"]
     total_heating_supply = (
         results[heat_pump_heating_rate] + results[water_pump_heating_rate]
     )
@@ -105,28 +120,105 @@ def test_ABAB_heatflows_balance():
         + results[z3_heating_rate]
         + results[z4_heating_rate]
     )
+    total_baseboard_heating_rate = results[baseboard_heating_rate]
 
+    assert float_near(total_heating_demand, total_baseboard_heating_rate, 0.1)
     assert float_near(total_heating_supply, total_heating_demand, 1.0)
 
 
-def test_AAAA_room_temps():
-    results = read_eplus_csv("out-dd-AAAA/eplusout.csv")
+def test_AABB_WC_heatflows_balance():
+    results = sim_results["out-dd-AABB-WC"]
+    total_heating_supply = (
+        results[heat_pump_heating_rate] + results[water_pump_heating_rate]
+    )
+    total_heating_demand = (
+        results[z1_heating_rate]
+        + results[z2_heating_rate]
+        + results[z3_heating_rate]
+        + results[z4_heating_rate]
+    )
+    total_baseboard_heating_rate = results[baseboard_heating_rate]
+
+    assert float_near(total_heating_demand, total_baseboard_heating_rate, 0.1)
+    assert float_near(total_heating_supply, total_heating_demand, 1.0)
+
+
+def test_ABAB_LC_heatflows_balance():
+    results = sim_results["out-dd-ABAB-LC"]
+    total_heating_supply = (
+        results[heat_pump_heating_rate] + results[water_pump_heating_rate]
+    )
+    total_heating_demand = (
+        results[z1_heating_rate]
+        + results[z2_heating_rate]
+        + results[z3_heating_rate]
+        + results[z4_heating_rate]
+    )
+    total_baseboard_heating_rate = results[baseboard_heating_rate]
+
+    assert float_near(total_heating_demand, total_baseboard_heating_rate, 0.1)
+    assert float_near(total_heating_supply, total_heating_demand, 1.0)
+
+
+def test_ABAB_WC_heatflows_balance():
+    results = sim_results["out-dd-ABAB-WC"]
+    total_heating_supply = (
+        results[heat_pump_heating_rate] + results[water_pump_heating_rate]
+    )
+    total_heating_demand = (
+        results[z1_heating_rate]
+        + results[z2_heating_rate]
+        + results[z3_heating_rate]
+        + results[z4_heating_rate]
+    )
+    total_baseboard_heating_rate = results[baseboard_heating_rate]
+
+    assert float_near(total_heating_demand, total_baseboard_heating_rate, 0.1)
+    assert float_near(total_heating_supply, total_heating_demand, 1.0)
+
+
+def test_AAAA_LC_room_temps():
+    results = sim_results["out-dd-AAAA-LC"]
     assert float_near(results[z1_air_temp], A_ROOM_TEMP_C, 0.01)
     assert float_near(results[z2_air_temp], A_ROOM_TEMP_C, 0.01)
     assert float_near(results[z3_air_temp], A_ROOM_TEMP_C, 0.01)
     assert float_near(results[z4_air_temp], A_ROOM_TEMP_C, 0.01)
 
 
-def test_AABB_room_temps():
-    results = read_eplus_csv("out-dd-AABB/eplusout.csv")
+def test_AAAA_WC_room_temps():
+    results = sim_results["out-dd-AAAA-WC"]
+    assert float_near(results[z1_air_temp], A_ROOM_TEMP_C, 0.01)
+    assert float_near(results[z2_air_temp], A_ROOM_TEMP_C, 0.01)
+    assert float_near(results[z3_air_temp], A_ROOM_TEMP_C, 0.01)
+    assert float_near(results[z4_air_temp], A_ROOM_TEMP_C, 0.01)
+
+
+def test_AABB_LC_room_temps():
+    results = sim_results["out-dd-AABB-LC"]
+    assert float_near(results[z1_air_temp], A_ROOM_TEMP_C, 0.01)
+    assert float_near(results[z2_air_temp], A_ROOM_TEMP_C, 0.01)
+    assert float_near(results[z3_air_temp], B_ROOM_TEMP_C, 0.01)
+    assert float_near(results[z4_air_temp], B_ROOM_TEMP_C, 0.01)
+
+
+def test_AABB_WC_room_temps():
+    results = sim_results["out-dd-AABB-WC"]
     assert float_near(results[z1_air_temp], A_ROOM_SAG_C, 0.5)
     assert float_near(results[z2_air_temp], A_ROOM_SAG_C, 0.5)
     assert float_near(results[z3_air_temp], B_ROOM_TEMP_C, 0.01)
     assert float_near(results[z4_air_temp], B_ROOM_TEMP_C, 0.01)
 
 
-def test_ABAB_room_temps():
-    results = read_eplus_csv("out-dd-ABAB/eplusout.csv")
+def test_ABAB_LC_room_temps():
+    results = sim_results["out-dd-ABAB-LC"]
+    assert float_near(results[z1_air_temp], A_ROOM_TEMP_C, 0.01)
+    assert float_near(results[z2_air_temp], B_ROOM_TEMP_C, 0.01)
+    assert float_near(results[z3_air_temp], B_ROOM_TEMP_C, 0.01)
+    assert float_near(results[z4_air_temp], A_ROOM_TEMP_C, 0.01)
+
+
+def test_ABAB_WC_room_temps():
+    results = sim_results["out-dd-ABAB-WC"]
     assert float_near(results[z1_air_temp], A_ROOM_SAG_C, 0.5)
     assert float_near(results[z2_air_temp], B_ROOM_TEMP_C, 0.01)
     assert float_near(results[z3_air_temp], B_ROOM_TEMP_C, 0.01)
