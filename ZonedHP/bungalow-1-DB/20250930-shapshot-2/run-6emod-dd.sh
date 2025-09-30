61f64ea016fbdd6e878d7d28cc9ac86eedf5ebf7
DamonHD <d@hd.org>
2025-09-30 16:37:41 +0100

    Captured reduced heat loss with correct DD parameters.

---
+++ ZonedHP/bungalow-1-DB/20250930-shapshot-2/run-6emod-dd.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Run IDF for winter design day.
+
+#if [ "" = "${WEATHERDIR}" ]; then
+#    echo "ERROR: please set WEATHERDIR" 1>&2
+#    exit 1
+#fi
+
+exec /Applications/EnergyPlus-9-4-0/energyplus -d out-6emod-dd -D 6emod.idf
