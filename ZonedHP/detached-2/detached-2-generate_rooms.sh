3929ff4b743b15a72c4f34929154054ee67d1817
DamonHD <d@hd.org>
2026-05-18 16:09:21 +0100

    WIP - all tests pass, but shouldn't!

--- ZonedHP/detached-2/detached-2-generate_rooms.sh
+++ ZonedHP/detached-2/detached-2-generate_rooms.sh
@@ -34,9 +34,9 @@ WC_CONTROL_SCHEME=WeatherCompHeatPumpController
 # DHD20260407: bungalow-2 values RAD_UA_FACTOR=30.5 RAD_FLOW_MAX=0.03e-3
 RAD_FLOW_MAX=0.03e-3
 # DHD20260515: shrunk by ~1.87x to reflect reduced heat loss downstairs.
-RAD_UA_FACTOR=16.31
+RAD_UA_FACTOR=35   # 16.31 # nominally 16.31 w/ RAD2@30.50.
 # DHD20260515: second (upstairs) radiator size; same as for bungalow-2.
-RAD2_UA_FACTOR=30.50
+RAD2_UA_FACTOR=90  # 30.51
 
 # Design day outside air temperature
 #
