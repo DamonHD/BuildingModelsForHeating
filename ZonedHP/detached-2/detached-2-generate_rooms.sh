7b83dbc5d46c3add499eaebd12d8ceb82b286814
DamonHD <d@hd.org>
2026-05-18 16:41:46 +0100

    WIP - minimised RAD_UA_FACTOR

--- ZonedHP/detached-2/detached-2-generate_rooms.sh
+++ ZonedHP/detached-2/detached-2-generate_rooms.sh
@@ -34,7 +34,7 @@ WC_CONTROL_SCHEME=WeatherCompHeatPumpController
 # DHD20260407: bungalow-2 values RAD_UA_FACTOR=30.5 RAD_FLOW_MAX=0.03e-3
 RAD_FLOW_MAX=0.03e-3
 # DHD20260515: heat loss per room nominally ~1.870x higher upstairs.
-RAD_UA_FACTOR=35   # 16.31 # nominally 16.31 w/ RAD2@30.50.
+RAD_UA_FACTOR=33   # 16.31 # nominally 16.31 w/ RAD2@30.50.
 # DHD20260515: second (upstairs) radiator size; same as for bungalow-2.
 RAD2_UA_FACTOR=90  # 30.51
 
