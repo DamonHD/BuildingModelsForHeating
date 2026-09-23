b97aa9f20de8bff32af59977261b3fcd18290b6e
DamonHD <d@hd.org>
2026-09-23 14:31:31 +0100

    WIP...

--- ZonedHP/midterrace-2/midterraced-2-runall-dd.sh
+++ ZonedHP/midterrace-2/midterraced-2-runall-dd.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 printf "Running AAAA model with load compensation\n"
-energyplus-24.2.0 -r -d out-dd-AAAA-LC -D detached-2-heatpump-AAAA-LoadCompHeatPumpController.idf
+energyplus-24.2.0 -r -d out-dd-AAAA-LC -D midterrace-2-heatpump-AAAA-LoadCompHeatPumpController.idf
 
 printf "Running ABAB model with load compensation\n"
-energyplus-24.2.0 -r -d out-dd-ABAB-LC -D detached-2-heatpump-ABAB-LoadCompHeatPumpController.idf
+energyplus-24.2.0 -r -d out-dd-ABAB-LC -D midterrace-2-heatpump-ABAB-LoadCompHeatPumpController.idf
 
 printf "Running AABB model with load compensation\n"
-energyplus-24.2.0 -r -d out-dd-AABB-LC -D detached-2-heatpump-AABB-LoadCompHeatPumpController.idf
+energyplus-24.2.0 -r -d out-dd-AABB-LC -D midterrace-2-heatpump-AABB-LoadCompHeatPumpController.idf
 
 printf "Running AAAA model with weather compensation\n"
-energyplus-24.2.0 -r -d out-dd-AAAA-WC -D detached-2-heatpump-AAAA-WeatherCompHeatPumpController.idf
+energyplus-24.2.0 -r -d out-dd-AAAA-WC -D midterrace-2-heatpump-AAAA-WeatherCompHeatPumpController.idf
 
 printf "Running ABAB model with weather compensation\n"
-energyplus-24.2.0 -r -d out-dd-ABAB-WC -D detached-2-heatpump-ABAB-WeatherCompHeatPumpController.idf
+energyplus-24.2.0 -r -d out-dd-ABAB-WC -D midterrace-2-heatpump-ABAB-WeatherCompHeatPumpController.idf
 
 printf "Running AABB model with weather compensation\n"
-energyplus-24.2.0 -r -d out-dd-AABB-WC -D detached-2-heatpump-AABB-WeatherCompHeatPumpController.idf
+energyplus-24.2.0 -r -d out-dd-AABB-WC -D midterrace-2-heatpump-AABB-WeatherCompHeatPumpController.idf
